ea345de532e44f79b6cc30931d538c77fa486831
ashwinimore808@gmail.ocm <ashwinimore808@gmail.com>
2023-04-27 11:36:10 +0530

    7th commit

--- src/main/java/com/example/EComApplication/brand/BrandController.java
+++ src/main/java/com/example/EComApplication/brand/BrandController.java
@@ -68,7 +68,7 @@ public class BrandController
   ///// get all brands which category present in brand
 	
     @GetMapping("/getcategory_id/{id}")
-    public ResponseEntity<List<Brand>> getBrandByCategoryId(@PathVariable long id,Long salary)
+    public ResponseEntity<List<Brand>> getBrandByCategoryId(@PathVariable long id,long s )
     {
     	try
     	{
